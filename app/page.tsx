325aee9a1b5851b1551dc3cda72fa50d0a795252
andy82115 <andy82115@gmail.com>
2024-12-08 16:23:03 +0900

    スタートボタンのホバーアニメーション

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 import { Routes } from './util/router/routes'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   const router = useRouter()
@@ -12,15 +13,22 @@ export default function Home() {
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <Button
-        className="w-fit h-fit"
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
+      <motion.button
+        className="w-fit h-fit px-20 py-5 text-2xl text-black bg-blue-200 font-semibold hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 rounded-full"
         onClick={() => {
           pushSearchPage()
         }}
+        whileHover={{
+          scale: 1.5, // Scale up on hover
+          transition: { type: 'spring', stiffness: 300, damping: 20 } // Smooth animation
+        }}
+        whileTap={{
+          scale: 0.95, // Slight scale down on tap for interaction feedback
+        }}
       >
-        Goooooo Search Repository
-      </Button>
+        Start
+      </motion.button>
     </div>
   )
 }
